430311c423e0bb031a445266c6403d9a0c421365
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2025-10-01 09:13:22 +0200

    --- MÉTHODES DE CONVERSION ---

--- tests.ts
+++ tests.ts
@@ -141,18 +141,18 @@ penguins.slice(0, 3).forEach((p, index) => {
 });
 
 
-// // ===== MÉTHODES DE CONVERSION =====
+// ===== MÉTHODES DE CONVERSION =====
 
-// console.log("--- MÉTHODES DE CONVERSION ---");
+console.log("--- MÉTHODES DE CONVERSION ---");
 
-// // join() - Joint tous les éléments en une chaîne
-// const premiersNoms = penguins.slice(0, 5).map(p => p.species);
-// console.log("• join() - Espèces séparées par ' | ':", premiersNoms.join(" | "));
-// console.log("• join() - Espèces séparées par des virgules:", /* TODO */);
+// join() - Joint tous les éléments en une chaîne
+const premiersNoms = penguins.slice(0, 5).map(p => p.species);
+console.log("• join() - Espèces séparées par ' | ':", premiersNoms.join(" | "));
+console.log("• join() - Espèces séparées par des virgules:", premiersNoms.join(", "));
 
-// // toString() - Convertit en chaîne (équivalent à join(','))
-// console.log("• toString() - Premières masses:", penguins.slice(0, 3).map(p => p.body_mass_g).toString());
-// console.log();
+// toString() - Convertit en chaîne (équivalent à join(','))
+console.log("• toString() - Premières masses:", penguins.slice(0, 3).map(p => p.body_mass_g).toString());
+console.log("• join() - Premières masses à join", penguins.slice(0 , 3).map(p => p.body_mass_g).join(", "));
 
 // // ===== MÉTHODES DE CONCATÉNATION =====
 
